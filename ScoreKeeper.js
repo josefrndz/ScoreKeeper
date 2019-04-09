ee6682a3c3e39e3d5eacfe55ef25077b2d3dca3d
Jose Fernandez <jfernandez@Joses-MacBook-Pro.local>
2019-04-08 23:27:40 -0400

    Added score keeping functionality to buttons

--- ScoreKeeper.js
+++ ScoreKeeper.js
@@ -1,1 +1,16 @@
-console.log("CONNECTED!");
+var p1Button = document.querySelector("#p1");
+var p2Button = document.querySelector("#p2");
+var p1Display = document.querySelector("#p1Display");
+var p2Display = document.querySelector("#p2Display");
+var p1Score = 0;
+var p2Score = 0;
+
+p1Button.addEventListener("click", function() {
+  p1Score++;
+  p1Display.textContent = p1Score;
+});
+
+p2Button.addEventListener("click", function() {
+  p2Score++;
+  p1Display.textContent = p2Score;
+});
